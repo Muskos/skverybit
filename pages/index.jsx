4099d3460327ca56618f163dd83bdb86e1a136fe
Aliaksei <a.d.makas@gmail.com>
2020-01-19 11:25:03 +0300

    Added favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,6 +26,12 @@ const Index = () => <>
         <meta property="og:title" content="Грушевский сквер в опасности" />
         <meta property="og:image" content={image} />
         <title>Грушевский сквер в опасности</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png" />
+        <link rel="mask-icon" href="../favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
     </Head>
     <LazyLoad {...LAZY_LOAD_CONFIG}>
         <MainSection />
